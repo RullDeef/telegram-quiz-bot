aa7324fea857c84322fd1bce3d3f9c57ebbf8a2d
Rull Deef <deeroll666@gmail.com>
2023-04-10 13:00:04 +0300

    add: create db script

--- db/init/00_create_database.sql
+++ db/init/00_create_database.sql
@@ -1,1 +1,39 @@
 create database quizdb;
+
+-- connect to created database
+\c quizdb
+
+create table users (
+    id serial,
+    nickname text,
+    telegram_id text,
+    role text
+);
+
+create table quizzes (
+    id serial,
+    topic text,
+    creator_id integer,
+    created_at timestamp
+);
+
+create table questions (
+    id serial,
+    "text" text
+);
+
+create table answers (
+    id serial,
+    question_id integer,
+    "text" text,
+    is_correct boolean
+);
+
+create table statistics (
+    user_id integer,
+    quizzes_completed integer,
+    mean_quiz_complete_time real,
+    mean_question_reply_time real,
+    correct_replies integer,
+    correct_replies_percent integer
+);
