128f0eb0deef690664e3b510f80280980af39f43
Lev <lev_trtykov@mail.ru>
2023-04-14 11:34:02 +0300

    feature: update AnswerRepository interface method name

--- bot/app/model/answer_repo.go
+++ bot/app/model/answer_repo.go
@@ -2,7 +2,7 @@ package model
 
 type AnswerRepository interface {
 	Create(answer Answer) error
-	Get(id int64) (Answer, error)
+	FindByAnswerId(id int64) (Answer, error)
 	Update(answer Answer) error
 	Delete(id int64) error
 }
