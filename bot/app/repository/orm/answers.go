a6926eba8165eedeaccada3d0f8cf1311a6625b1
Lev <lev_trtykov@mail.ru>
2023-04-14 11:36:48 +0300

    feature: set struct AnswerRepositoryStruct

--- bot/app/repository/orm/answers.go
+++ bot/app/repository/orm/answers.go
@@ -7,26 +7,25 @@ import (
 
 //New version
 type AnswerRepositoryStruct struct {
-	Db      *gorm.DB
-	LastId  int64
-	Quizzes []model.Quiz
+	Db     *gorm.DB
+	LastId int64
 }
 
-func (qr *AnswerRepositoryStruct) Get(id int64) (model.Answer, error) {
+func (ar *AnswerRepositoryStruct) FindByAnswerId(id int64) (model.Answer, error) {
 	var answer model.Answer
-	result := qr.Db.Find(&answer, id)
+	result := ar.Db.Find(&answer, id)
 
 	return answer, result.Error
 }
 
-func (qr *AnswerRepositoryStruct) Create(answer model.Answer) error {
-	return qr.Db.Table("answers").Create(answer).Error
+func (ar *AnswerRepositoryStruct) Create(answer model.Answer) error {
+	return ar.Db.Table("answers").Create(answer).Error
 }
 
-func (qr *AnswerRepositoryStruct) Update(answer model.Answer) error {
-	return qr.Db.Table("answers").Where("id = ?", answer.ID).Save(&answer).Error
+func (ar *AnswerRepositoryStruct) Update(answer model.Answer) error {
+	return ar.Db.Table("answers").Where("id = ?", answer.ID).Save(&answer).Error
 }
 
-func (qr *AnswerRepositoryStruct) Delete(id int64) error {
-	return qr.Db.Table("answers").Delete(&model.Answer{}, id).Error
+func (ar *AnswerRepositoryStruct) Delete(id int64) error {
+	return ar.Db.Table("answers").Delete(&model.Answer{}, id).Error
 }
