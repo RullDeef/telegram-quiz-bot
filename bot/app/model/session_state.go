538ec2d36ae731120cc41b9d8b467ff31f57414d
Rull Deef <deeroll666@gmail.com>
2023-04-30 19:10:43 +0300

    Разработать SessionController #43
    Реализован основной функционал контроллера сессий. Проведено ручное тестирование разработанных методов.

--- bot/app/model/session_state.go
+++ bot/app/model/session_state.go
@@ -1,10 +1,16 @@
 package model
 
+import "sync"
+
 type SessionState struct {
 	Quiz            Quiz
 	Users           []*User
 	CurrentQuestion *Question
-	IsPaused        bool
+	isPaused        bool
+	mutex           *sync.RWMutex
+
+	pauseChan  chan struct{}
+	resumeChan chan struct{}
 }
 
 func NewSessionState(q Quiz, users []*User) *SessionState {
@@ -12,6 +18,38 @@ func NewSessionState(q Quiz, users []*User) *SessionState {
 		Quiz:            q,
 		Users:           users,
 		CurrentQuestion: &q.Questions[0],
-		IsPaused:        false,
+		isPaused:        false,
+		mutex:           &sync.RWMutex{},
+
+		pauseChan:  make(chan struct{}, 1),
+		resumeChan: make(chan struct{}, 1),
 	}
+}
+
+func (s *SessionState) WaitForPause() chan struct{} {
+	return s.pauseChan
+}
+
+func (s *SessionState) WaitForResume() chan struct{} {
+	return s.resumeChan
+}
+
+func (s *SessionState) Pause() {
+	s.pauseChan <- struct{}{}
+	s.mutex.Lock()
+	s.isPaused = true
+	s.mutex.Unlock()
+}
+
+func (s *SessionState) Resume() {
+	s.resumeChan <- struct{}{}
+	s.mutex.Lock()
+	s.isPaused = false
+	s.mutex.Unlock()
+}
+
+func (s *SessionState) IsPaused() bool {
+	s.mutex.RLock()
+	defer s.mutex.RUnlock()
+	return s.isPaused
 }
